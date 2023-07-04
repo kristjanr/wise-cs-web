d5e9953c95fa940a02f43c4a85c9d39faecfe2fa
Kris <kristjan.roosild@pactum.com>
2023-07-05 02:28:33 +0300

    option to give feedback

--- app.py
+++ app.py
@@ -38,6 +38,35 @@ conn = psycopg2.connect(
 )
 
 @cross_origin()
+@app.route('/feedback', methods=['POST'])
+def feedback():
+    session_existed_already = set_session_if_needed()
+    if not session_existed_already:
+        return 'No session exists', 400
+    session_id = session['ID']
+    request_data = request.get_json()
+    row_id = request_data['messageId']
+    select_query = '''
+        SELECT session_id FROM session_history
+        WHERE id = %s AND session_id = %s AND is_good_feedback IS NULL
+    '''
+    with conn.cursor() as cursor:
+        cursor.execute(select_query, (row_id, session_id))
+        result = cursor.fetchone()
+        if result is None:
+            return 'Invalid messageId', 400
+    is_good_feedback = request_data['feedback'] == 'good'
+    feedback = request_data['additionalFeedback']
+    update_query = '''
+        UPDATE session_history
+        SET is_good_feedback = %s, feedback = %s
+        WHERE id = %s
+    '''
+    with conn.cursor() as cursor:
+        cursor.execute(update_query, (is_good_feedback, feedback, row_id))
+        conn.commit()
+    return 'Success', 200
+@cross_origin()
 @app.route('/logout')
 def logout():
     resp = make_response("Cookie Removed")
@@ -61,7 +90,8 @@ def answer():
     except openai.error.InvalidRequestError as e:
         n_tokens_used = e.user_message.split('your messages resulted in ')[1].split(' tokens')[0]
         return dict(answer='Context window full. Please reset session!', urls_used=[], n_tokens_used=n_tokens_used)
-
+    except openai.error.ServiceUnavailableError as e:
+        return dict(answer='OpenAI API is currently unavailable. Please try again later.', urls_used=[], n_tokens_used=0)
 
     questions = previous_questions.copy()
     questions.append(question)
@@ -69,10 +99,10 @@ def answer():
     messages = previous_messages.copy()
     messages.extend(new_messages)
 
-    save_to_database(session_id, question, llm_answer, urls, messages, questions)
+    row_id = save_to_database(session_id, question, llm_answer, urls, messages, questions)
 
     print(f'Question: {question}, Answer: {llm_answer}, Articles used: {"/n".join(urls)}"')
-    return dict(answer=llm_answer, urls_used=urls, n_tokens_used=n_tokens_used)
+    return dict(answer=llm_answer, urls_used=urls, n_tokens_used=n_tokens_used, id=row_id)
 
 
 def get_previous_questions(session_id: str) -> list:
@@ -111,10 +141,21 @@ def save_to_database(session_id, question, llm_answer, urls, messages, questions
     insert_query = '''
         INSERT INTO session_history (session_id, question, llm_answer, urls, messages, questions)
         VALUES (%s, %s, %s, %s, %s, %s)
+        RETURNING id
     '''
     with conn.cursor() as cursor:
         cursor.execute(insert_query, (session_id, question, llm_answer, json.dumps(urls), json.dumps(messages), json.dumps(questions)))
-        conn.commit()
+        row = cursor.fetchone()
+        if row:
+            row_id = row[0]
+            print(f'Inserted row with id {row_id}')
+            conn.commit()
+            return row_id
+        else:
+            # Handle the case where no row was returned
+            print('Failed to retrieve the inserted row ID.')
+            conn.rollback()
+            return None
 
 
 def set_session_if_needed():
